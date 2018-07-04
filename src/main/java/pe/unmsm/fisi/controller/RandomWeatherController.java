eae459e8028c1cca1b082b6f0cf2fd687edfea92
alonso.reynoso <alonso.reynoso@bbva.com>
2018-07-04 18:32:29 -0500

    feat(*): added initial call

--- src/main/java/pe/unmsm/fisi/controller/RandomWeatherController.java
+++ src/main/java/pe/unmsm/fisi/controller/RandomWeatherController.java
@@ -1,6 +1,10 @@
 package pe.unmsm.fisi.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
@@ -19,5 +23,11 @@ public class RandomWeatherController {
 	@Scheduled(initialDelay = 0, fixedRate = 60000)
 	public void index() {
 		template.convertAndSend("/topic/weather", statService.getRandomStat());
+	}
+	
+	@MessageMapping("/weather")
+	@SendTo("/topic/weather")
+	public Map<String, Object> initialData() {
+		return statService.getRandomStat();
 	}
 }
--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -9,5 +9,6 @@ window.onload = () => {
 			const {temperature, precipitation} = JSON.parse(weather.body).data;
 			document.getElementById('websocketPlaceholder').innerText = `Temperatura: Pais -> ${temperature.countryCode} Mes -> ${temperature.month} Valor -> ${temperature.stat} Precipitación: Pais -> ${precipitation.countryCode} Mes -> ${precipitation.month} Valor -> ${precipitation.stat}`;
 		});
+		stompClient.send("/app/weather", {}, JSON.stringify({'name': 'alonso'}));
 	});
 };
