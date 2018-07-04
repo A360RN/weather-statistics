5815a4d3d300a7b1bd7c72f8454082f1717cba43
alonso.reynoso <alonso.reynoso@bbva.com>
2018-07-03 20:46:25 -0500

    Initial commit

---
+++ src/main/java/pe/unmsm/fisi/WeatherStatisticsHeroku/WeatherStatisticsHerokuApplication.java
@@ -0,0 +1,12 @@
+package pe.unmsm.fisi.WeatherStatisticsHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStatisticsHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStatisticsHerokuApplication.class, args);
+	}
+}
---
+++ src/test/java/pe/unmsm/fisi/WeatherStatisticsHeroku/WeatherStatisticsHerokuApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.unmsm.fisi.WeatherStatisticsHeroku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherStatisticsHerokuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
